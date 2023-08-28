d35747795acd1db566e55bb1cf8d4866e67fb34e
Michael Liddle <miliddle@microsoft.com>
2023-08-28 17:06:36 -0600

    added IP address that is always correct based on testing

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -44,7 +44,7 @@ function getIPAddress() {
     .then(response => response.json())
     .then(data => {
       let ipString = "Accessing site from: ";
-      if (data["ipAddress"] == "##PROXY_IP##") {
+      if (data["ipAddress"] == "10.0.0.5") {
         document.getElementById("ip_card").classList.add("bg-success");
         document.getElementById("ip_address_span").innerHTML=ipString + data["ipAddress"] + " (Proxy)";
       } else {
